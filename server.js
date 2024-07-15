ea7dfb8e70e58146cb8cb43578a43f6e0751cb59
Arcaneghost24 <jmpresto1@yahoo.com>
2024-07-15 12:05:19 -0400

    commit before update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 
 // Set up sessions with cookies
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'Excalibur',
   cookie: {
     // Stored in milliseconds
     maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
