30d0b445782881a451fe891028dcbdf8c87cf2f6
cowboytrex <careesechow@gmail.com>
2024-07-17 00:23:37 +0800

    sign-up.js

--- models/signup.js
+++ models/signup.js
@@ -1,20 +1,23 @@
 
 document.querySelector('#signup-form').addEventListener('submit', async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const name = document.querySelector('#fullName').value.trim();
-    const email = document.querySelector('#email').value.trim();
-    const password = document.querySelector('#password').value.trim();
-    const userType = document.querySelector('#userType').value;
+  const name = document.querySelector('#fullName').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
+  const userType = document.querySelector('#userType').value;
 
-    if (name && email && password && userType) {
-        const response = await fetch('/api/users/signup', {
-            method: 'POST',
+  if (name && email && password && userType) {
+    const response = await fetch('/api/users/signup', {
+      method: 'POST',
       body: JSON.stringify({ name, email, password, userType }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-        document.location.replace('/profile');
+      document.location.replace('/profile');
     } else {
-        alert('Failed to sign up.');
+      alert('Failed to sign up.');
+    }
+  }
+});
