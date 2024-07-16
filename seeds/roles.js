4fcc6d141bd3ac744f8abb4b504c47cf675ac004
cowboytrex <careesechow@gmail.com>
2024-07-16 23:25:27 +0800

    roles.js

--- seeds/roles.js
+++ seeds/roles.js
@@ -3,11 +3,25 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Role extends Model {}
-Role.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
+
+    Role.init(
+        {
+          id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          role_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+        },
+        {
+          sequelize,
+          timestamps: false,
+          freezeTableName: true,
+          underscored: true,
+          modelName: 'role',
+        }
+      );
