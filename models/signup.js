fb0431fda0d648b8104b9e47b71aa35f54701800
cowboytrex <careesechow@gmail.com>
2024-07-17 00:21:02 +0800

    sign-up.js

--- models/signup.js
+++ models/signup.js
@@ -3,4 +3,6 @@ document.querySelector('#signup-form').addEventListener('submit', async (event) 
     event.preventDefault();
 
     const name = document.querySelector('#fullName').value.trim();
-  const email = document.querySelector('#email').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    const userType = document.querySelector('#userType').value;
