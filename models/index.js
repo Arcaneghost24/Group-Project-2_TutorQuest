e0f26bf35ee63b3ea458c100a2261580a4131156
Arcaneghost24 <jmpresto1@yahoo.com>
2024-07-18 10:15:48 -0400

    commit before update

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const Subject = require('./Subject');
 const TutorSession = require('./Tutor_session');
 const Role = require('./Role');
 
-User.hasOne(Role, { as: 'role' });
+User.hasOne(Role, { as: 'userRole' });
 // User.hasMany(TutorSession, { foreignKey: 'user_id' });
 
 // TutorSession.belongsTo(User, { foreignKey: 'user_id' });
