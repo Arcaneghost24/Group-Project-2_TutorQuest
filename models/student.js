81a432bb5432801b9bf23e5bb7b6231005c63f77
marcmarino83 <marc00marino@gmail.com>
2024-07-18 08:37:49 -0400

    changed student model to fit data

--- models/student.js
+++ models/student.js
@@ -15,13 +15,65 @@ Student.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    subject: {
+    profilePicture: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    grade: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    currentSubjects: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    academicGoals: {
       type: DataTypes.TEXT,
       allowNull: false,
+    },
+    preferredSubjects: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    preferredTime: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    modeOfTutoring: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    upcomingSessions: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true,
+    },
+    pastSessions: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true,
+    },
+    messages: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    notesResources: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    paymentHistory: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true,
+    },
+    paymentMethods: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
     user_id: {
       type: DataTypes.INTEGER,
