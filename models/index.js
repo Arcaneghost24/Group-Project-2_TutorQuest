630d4e5f16c0a3cd833444c1cb33cf29332ee52d
Arcaneghost24 <jmpresto1@yahoo.com>
2024-07-18 11:22:54 -0400

    update

--- models/index.js
+++ models/index.js
@@ -4,9 +4,9 @@ const TutorSession = require('./Tutor_session');
 const Role = require('./Role');
 
 User.hasOne(Role, { as: 'userRole' });
-User.hasMany(TutorSession, { foreignKey: 'user_id' });
+// User.hasMany(TutorSession, { foreignKey: 'user_id' });
 
-TutorSession.belongsTo(User, { foreignKey: 'user_id' });
+// TutorSession.belongsTo(User, { foreignKey: 'user_id' });
 
 Subject.belongsToMany(User, {
   through: {
@@ -14,7 +14,7 @@ Subject.belongsToMany(User, {
   },
 });
 
-Tutor.belongsTo(User, { foreignKey: 'roletype' });
-Student.belongsTo(User, { foreignKey: 'roletype' });
+// Tutor.belongsTo(User, { foreignKey: 'roletype' });
+// Student.belongsTo(User, { foreignKey: 'roletype' });
 
 module.exports = { User, Subject, TutorSession };
